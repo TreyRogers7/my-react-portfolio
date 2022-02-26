66235e7226ee54069dd395e28444591dbe2a1549
Richard Rogers <treyrogers073@gmail.com>
2022-02-25 17:55:19 -0700

    starting over, angry. will finish and graduate thogit add .git add .git add .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/home/home";
-import Navbar from "./components/nav";
-import Contact from "./pages/contact/contact.js";
-import Projects from "./pages/projects/projects";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  document.title = "Trey Rogers";
-  return (
-    <Router>
-    <div className= "origin">
-      <Navbar />
-      <Wrapper>
-        <Container className ="align-content-center mt-4">
-          <Row className= "justify-content-center">
-            <Col className= "justify-content-center" xl ={9} md= {10} xs= {11}>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/portfolio" component={Projects} />
-            <Route exact path="/contact" component={Contact} />
-            </Col>
-          </Row>
-        </Container>
-      </Wrapper>
-      <Footer />
-    </div>
-  </Router>
-  );
-}
+const App = () => <PortfolioContainer/>;
 
 export default App;
